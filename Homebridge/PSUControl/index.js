e0361e282f3fe2320975aa98759b73a471eaa3e2
drewcassidy <drewcassidy@me.com>
2018-02-22 16:26:45 -0800

    Begin writing homebridge plugin • add start of homebridge plugin implementation

--- Homebridge/PSUControl/index.js
+++ Homebridge/PSUControl/index.js
@@ -12,3 +12,45 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+const Service, Characteristic;
+
+//initialize module and types
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory("psu-switch-plugin", "3D Printer PSU", psuSwitch);
+}
+
+function psuSwitch(log, config) {
+    this.log = log;
+    this.name = config['name'];
+    this.socket = config['socket'];
+    
+}
+
+psuSwitch.prototype.getServices = function() {
+    let informationService = new Service.AccessoryInformation();
+    informationService
+        .setCharacteristic(Characteristic.Manufacturer, "Andrew Cassidy")
+        .setCharacteristic(Characteristic.Model, "3D Printer PSU controller")
+        .setCharacteristic(Characteristic.SerialNumber, "987-654-321")
+
+    let switchService = new Service.Switch("Power Switch");
+    switchService
+        .getCharacteristic(Characteristic.On)
+            .on('get', this.getOn.bind(this))
+            .on('set', this.setOn.bind(this))
+
+    this.informationService = informationService;
+    this.switchService = switchService;
+    return [informationService, switchService];
+}
+
+psuSwitch.prototype.getOn = function(next) {
+    //get the thing
+}
+
+psuSwitch.prototype.setOn = function(on, next) {
+    //do the thing
+}
